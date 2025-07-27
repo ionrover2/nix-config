057433d1f93b33080361edf7fbb9b6439d7e1156
Some One <someone@example.com>
2025-07-27 13:01:32 -0500

    whatever

--- flake.nix
+++ flake.nix
@@ -8,11 +8,35 @@
   };
 
   outputs =
-    inputs@{ self,nixpkgs, home-manager }:
+    inputs@{
+      self,
+      nixpkgs,
+      home-manager,
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
     {
-
       nixosConfigurations = {
-        nixos = nixpkgs.lib.nixosSystem { modules = [ ./nixos ]; };
+        nixos = nixpkgs.lib.nixosSystem {
+          modules = [
+            ./nixos
+            home-manager.nixosModules
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.thall = ./home-manager;
+            }
+          ];
+        };
+      };
+      # Standalone home manager configurations.
+      homeConfigurations."thall" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        modules = [ ./home-manager ];
+
       };
     };
 }
